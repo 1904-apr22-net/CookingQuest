2150366f595f55dda1b58aec2cdd6edca8fee057
Paul <paul_grimes8@yahoo.com>
2019-05-20 18:23:49 -0500

    Added Repositories and IRepositories And injected the via Startup

---
+++ CookingQuest/CookingQuest.Library/IRepository/IPlayerRepo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CookingQuest.Library.IRepository
+{
+    public interface IPlayerRepo : IDisposable
+    {
+        void Save();
+    }
+}
---
+++ CookingQuest/CookingQuest.Library/IRepository/IStoreRepo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CookingQuest.Library.IRepository
+{
+    public interface IStoreRepo : IDisposable
+    {
+        void Save();
+    }
+}
