47cf9ae53124b1dcc0a688d946c4dbf938118b40
Paul <paul_grimes8@yahoo.com>
2019-05-20 11:02:30 -0500

    updtaed entities folder

--- CookingQuest/CookingQuest.Data/Entities/Account.cs
+++ CookingQuest/CookingQuest.Data/Entities/Account.cs
@@ -9,6 +9,7 @@ namespace CookingQuest.Data.Entities
         public string Username { get; set; }
         public string Password { get; set; }
         public int PlayerId { get; set; }
+        public bool IsAdmin { get; set; }
 
         public virtual Player Player { get; set; }
     }
